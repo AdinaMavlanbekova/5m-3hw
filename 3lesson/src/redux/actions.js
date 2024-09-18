b9d4c51214f5cdfc6a0b44dfad47de6221abca36
AdinaMavlanbekova <100846189+AdinaMavlanbekova@users.noreply.github.com>
2024-09-18 16:10:23 +0600

    Add files via upload

---
+++ 3lesson/src/redux/actions.js
@@ -0,0 +1,16 @@
+
+
+export function addUserAction(user) {
+    return async function () {
+
+        const options = {
+            method: 'POST',
+            headers: {
+                "Content-type": 'application/json'
+            },
+            body: JSON.stringify(user)
+        }
+
+        const response = await fetch('https://jsonplaceholder.typicode.com/users', options)
+    }
+ }
